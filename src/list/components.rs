3161705e621529654136c5fc5507a0d2b38924fb
Luigi Mario Zuccarelli <luzuccar@redhat.com>
2023-04-25 16:38:04 +0200

    Update to speed lookup for filter

--- src/list/components.rs
+++ src/list/components.rs
@@ -6,18 +6,19 @@ use std::fs;
 // list all components in the current image index
 pub async fn list_components(ctype: String, dir: String, filter: String) {
     let paths = fs::read_dir(&dir).unwrap();
-    for path in paths {
-        let entry = path.expect("could not resolve path entry");
-        let dir_name = entry.path();
-        let str_dir = dir_name.into_os_string().into_string().unwrap();
-        let res = str_dir.split("/");
-        let name = format!("{} => {}", ctype, res.into_iter().last().unwrap());
-        let dc = read_operator_catalog(str_dir);
-        if filter != "all" && name.contains(&filter) {
-            log_hi(&name);
-            list_channel_info(dc.unwrap());
-            break;
-        } else if filter == "all" {
+
+    if filter != "all" {
+        let dc = read_operator_catalog(dir + &"/".to_string() + &filter);
+        log_hi(&filter);
+        list_channel_info(dc.unwrap());
+    } else {
+        for path in paths {
+            let entry = path.expect("could not resolve path entry");
+            let dir_name = entry.path();
+            let str_dir = dir_name.into_os_string().into_string().unwrap();
+            let res = str_dir.split("/");
+            let name = format!("{} => {}", ctype, res.into_iter().last().unwrap());
+            let dc = read_operator_catalog(str_dir);
             log_hi(&name);
             list_channel_info(dc.unwrap());
         }
@@ -46,19 +47,19 @@ pub fn list_channel_info(dc: serde_json::Value) {
     for x in dc {
         if x.default_channel != None {
             let channel = x.default_channel.clone().unwrap();
-            log_lo(&format!("   defaultChannel => {}", channel));
+            log_lo(&format!("  defaultChannel => {}", channel));
         }
         if x.schema == "olm.channel" {
-            log_lo(&format!("   channel => {}", x.name));
+            log_lo(&format!("  channel => {}", x.name));
         }
         if x.schema == "olm.bundle" {
-            log_mid(&format!("     bundle => {}", x.name));
+            log_mid(&format!("    bundle => {}", x.name));
         }
     }
     for decl_entry in de.into_iter() {
         if let Some(channel_entry) = decl_entry.entries {
             for item in channel_entry {
-                log_lo(&format!("     entry => {}", item.name));
+                log_ex(&format!("    entry => {}", item.name));
             }
         }
     }
--- src/log/logging.rs
+++ src/log/logging.rs
@@ -2,29 +2,33 @@
 
 // logging convenience functions
 pub fn log_info(msg: &str) {
-    println!("\x1b[1;94m {} \x1b[0m  : {}","INFO",msg);
+    println!("\x1b[1;94m {} \x1b[0m  : {}", "INFO", msg);
 }
 
 pub fn log_debug(msg: &str) {
-    println!("\x1b[1;92m {} \x1b[0m : {}","DEBUG",msg);
+    println!("\x1b[1;92m {} \x1b[0m : {}", "DEBUG", msg);
 }
 
 pub fn log_hi(msg: &str) {
-    println!("\x1b[1;94m {}  \x1b[0m : \x1b[1;95m{} \x1b[0m","INFO",msg);
+    println!("\x1b[1;94m {}  \x1b[0m : \x1b[1;95m{} \x1b[0m", "INFO", msg);
 }
 
 pub fn log_mid(msg: &str) {
-    println!("\x1b[1;94m {}  \x1b[0m : \x1b[1;96m{} \x1b[0m","INFO",msg);
+    println!("\x1b[1;94m {}  \x1b[0m : \x1b[1;96m{} \x1b[0m", "INFO", msg);
 }
 
 pub fn log_lo(msg: &str) {
-    println!("\x1b[1;94m {}  \x1b[0m : \x1b[1;93m{} \x1b[0m","INFO",msg);
+    println!("\x1b[1;94m {}  \x1b[0m : \x1b[1;93m{} \x1b[0m", "INFO", msg);
+}
+
+pub fn log_ex(msg: &str) {
+    println!("\x1b[1;94m {}  \x1b[0m : \x1b[1;98m{} \x1b[0m", "INFO", msg);
 }
 
 pub fn log_warn(msg: &str) {
-    println!("\x1b[1;93m {} \x1b[0m  : {}","WARN",msg);
+    println!("\x1b[1;93m {} \x1b[0m  : {}", "WARN", msg);
 }
 
 pub fn log_error(msg: &str) {
-    println!("\x1b[1;91m {} \x1b[0m : {}","ERROR",msg);
+    println!("\x1b[1;91m {} \x1b[0m : {}", "ERROR", msg);
 }
